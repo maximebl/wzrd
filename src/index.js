25f976296570ee1d15ff21eb64a7e52ee1ac9e9c
maximebl <maximebl@videotron.ca>
2017-06-24 11:42:56 -0400

    scaffolding for three js

--- src/index.js
+++ src/index.js
@@ -1,2 +1,5 @@
 import './Components/SwipeableCard/cards.css';
+import './Components/SideNav/SideNav.css';
+import './Components/Viewport/viewport';
 import Cards from './Components/SwipeableCard/Cards';
+import SideNav from './Components/SideNav/side-nav';
